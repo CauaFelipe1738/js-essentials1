46e9f6e438dbe04934625028856514a9ac071c44
Cauã Felipe <kaua.carvalho.1bmangal@gmail.com>
2025-09-15 15:19:27 -0300

    Create 1.js

---
+++ m2/m2.1/1.js
@@ -0,0 +1,18 @@
+//Declaração de variáveis
+let s = 'Olá';
+let n = 4;
+let b = false;
+
+//Mostra o valor e seu tipo
+console.log(`${s}, `, typeof s);
+console.log(`${n}, `, typeof n);
+console.log(`${b}, `, typeof b);
+
+//Converte o número para texto
+n = toString(n);
+console.log(`${n}, `, typeof n);
+
+//converte numero para numero
+let x = 5.0;
+x = parseFloat(x);
+console.log(`${x}, `, typeof x);
